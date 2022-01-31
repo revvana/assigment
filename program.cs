f111c36ad091a041c9e4ea7c82ffa4c3b09f7e28
revvana <>
2022-01-31 15:46:01 +0300

    Finish

--- program.cs
+++ program.cs
@@ -1,4 +1,2 @@
-string[] CreateArrray = {"one","two","three","four","five"};
-{
-    return new string[array];
-}
+string[] CreateArrray = {"one","two","three","four","five","1","12","123","1234"};
+string[] ThreeSymbols = CreateArrray;
