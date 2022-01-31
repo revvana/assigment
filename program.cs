7d441ce2a970bc157ac2cf72465e42d83614e5ee
revvana <>
2022-01-31 16:08:31 +0300

    End

--- program.cs
+++ program.cs
@@ -1,2 +1,9 @@
 string[] CreateArrray = {"one","two","three","four","five","1","12","123","1234"};
 string[] ThreeSymbols = CreateArrray;
+for (int i = 0; i < ThreeSymbols.Count(); i++)
+{
+    if (ThreeSymbols[i].Length <= 3)
+    {
+        Console.WriteLine(ThreeSymbols[i]); 
+    }
+}
